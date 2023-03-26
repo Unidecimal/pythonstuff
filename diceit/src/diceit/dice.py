3ebdf8506ceee834b50c26da3f562f08f9d74f9c
Melker Holmgren <melker@funnel.io>
2023-03-26 14:45:46 +0200

    diceit: implementing tests

--- diceit/src/diceit/dice.py
+++ diceit/src/diceit/dice.py
@@ -7,15 +7,39 @@ n = 1
 k = 1
 
 # Totala antal sidor på tärningen
-tot_sides = 6
+dice_tot_sides = 6
 
 # Antal sidor på tärningen som ger en "framgång"
 successes_sides = 1
 
 # Beräkna sannolikheten med hjälp av binomialfördelningen
 prob = sum(
-    [math.comb(n, i) * (successes_sides/tot_sides)**i * (1-(successes_sides/tot_sides))**(n-i) 
+    [math.comb(n, i) * (successes_sides/dice_tot_sides)**i * (1-(successes_sides/dice_tot_sides))**(n-i) 
      for i in range(k, n+1)]
     )
 
-print(f"Sannolikhet att slå över {tot_sides - successes_sides} på minst {k} av {n} tärningar: {prob:.2%}")
+print(f"Sannolikhet att slå över {dice_tot_sides - successes_sides} på minst {k} av {n} tärningar: {prob:.2%}")
+
+def succsess_percentage_for_dice_roll(dice_n, success_dice_n, dice_tot_sides, successes_sides):
+
+    # Number of dices
+    dice_n = 1
+
+    # Number of succsesses needed
+    success_dice_n = 1
+
+    # Total number of sides on the dice
+    dice_tot_sides = 6
+
+    # The number of sides that result in succsess
+    successes_sides = 1
+
+    # Calculate the probability using the binomial distribution
+    prob = sum(
+        [math.comb(dice_n, i) * (successes_sides/dice_tot_sides)**i * (1-(successes_sides/dice_tot_sides))**(dice_n-i) 
+        for i in range(success_dice_n, dice_n+1)]
+        )
+    
+    return prob
+
+
--- diceit/test/test_dice.py
+++ diceit/test/test_dice.py
@@ -1,5 +1,7 @@
 import pytest
 
 
-def calculate_cances_of_multiple_diece_roll():
-    assert succsess_percentage_for_dice_roll()
+def test_calculate_cances_of_multiple_diece_roll():
+    result = succsess_percentage_for_dice_roll(dice, number, target)
+    assert result == 16.67
+
